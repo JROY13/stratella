dc9c23940ac6c004a580caab124d417f67fdb321
John Meissner <JROY13@users.noreply.github.com>
2025-08-19 13:16:18 -0600

    fix: replace missing markdown extension

--- src/components/editor/InlineEditor.tsx
+++ src/components/editor/InlineEditor.tsx
@@ -6,7 +6,7 @@ import StarterKit from '@tiptap/starter-kit'
 import TaskList from '@tiptap/extension-task-list'
 import TaskItem from '@tiptap/extension-task-item'
 import Placeholder from '@tiptap/extension-placeholder'
-import Markdown from '@tiptap/extension-markdown'
+import { Markdown } from 'tiptap-markdown'
 
 export interface InlineEditorProps {
   markdown: string
@@ -15,13 +15,7 @@ export interface InlineEditorProps {
 
 export default function InlineEditor({ markdown, onChange }: InlineEditorProps) {
   const editor = useEditor({
-    extensions: [
-      StarterKit,
-      TaskList,
-      TaskItem,
-      Placeholder,
-      Markdown,
-    ],
+    extensions: [StarterKit, TaskList, TaskItem, Placeholder, Markdown],
     editorProps: {
       attributes: {
         class: 'focus:outline-none',
@@ -31,22 +25,14 @@ export default function InlineEditor({ markdown, onChange }: InlineEditorProps) 
 
   React.useEffect(() => {
     if (editor && markdown) {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const parser = (editor.storage.markdown as any)?.parser
-      if (parser) {
-        editor.commands.setContent(parser(markdown))
-      }
+      editor.commands.setContent(markdown)
     }
   }, [editor, markdown])
 
   React.useEffect(() => {
     if (!editor || !onChange) return
     const handler = () => {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const serializer = (editor.storage.markdown as any)?.serializer
-      if (serializer) {
-        onChange(serializer(editor.getJSON()))
-      }
+      onChange(editor.storage.markdown.getMarkdown())
     }
     editor.on('update', handler)
     return () => {
